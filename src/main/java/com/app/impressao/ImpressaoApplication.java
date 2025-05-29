d5963395a62acb47e32a765e7d7cb1e0b27ae9c0
JoelMNeto <joelneto441@gmail.com>
2025-05-28 22:38:45 -0300

    commit inicial

---
+++ src/main/java/com/app/impressao/ImpressaoApplication.java
@@ -0,0 +1,13 @@
+package com.app.impressao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpressaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpressaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/impressao/ImpressaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.impressao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpressaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
